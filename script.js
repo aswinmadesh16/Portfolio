9f7e968afedff2146336793af49efaa73914b23e
aswinmadesh16 <aswinmadesh1998@gmail.com>
2026-06-17 21:52:21 +0530

    Updated portfolio content

--- script.js
+++ script.js
@@ -129,19 +129,3 @@ function prevSlide() {
 }
 
 initSlider();
-const backToTopBtn = document.getElementById("backToTop");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 250) {
-    backToTopBtn.classList.add("show");
-  } else {
-    backToTopBtn.classList.remove("show");
-  }
-});
-
-backToTopBtn.addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
